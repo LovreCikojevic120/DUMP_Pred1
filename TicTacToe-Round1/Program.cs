bf8a3a7904be0c8ac411c24b8ea271a1b3a97039
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2021-11-10 16:37:20 +0100

    Tic Tac Toe - DVOBOJ!

---
+++ TicTacToe-Round1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToe_Round1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bartol ima x pobjeda i y poraza!");
+            Console.WriteLine("Lovre ima z pobjeda i n poraza!");
+
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+        }
+    }
+}
---
+++ TicTacToe-Round2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicTacToe_Round2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bartol ima x pobjeda i y poraza!");
+            Console.WriteLine("Lovre ima z pobjeda i n poraza!");
+
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+
+        }
+    }
+}
